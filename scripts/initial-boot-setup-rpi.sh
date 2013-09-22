443b92059ab6079092c86206802f894233795f13
Alexander Rydekull <rydekull@gmail.com>
2013-09-22 13:51:11 +0200

    Eh, if sshd already exists in hosts.allow, remove it and readd it

--- scripts/initial-boot-setup-rpi.sh
+++ scripts/initial-boot-setup-rpi.sh
@@ -30,6 +30,8 @@ echo "ALL: ALL" >> /etc/hosts.deny
 
 # Fix hosts.allow
 NETWORK="$(/root/scripts/check-ipsubnet.sh $(ifconfig eth0 | awk '$0 ~ /Bcast/ { print $2, $NF }' | sed -e 's/addr://g' -e 's/Mask://g'))"
+grep -v ^sshd: /etc/hosts.allow > /etc/hosts.allow-new
+mv /etc/hosts.allow-new /etc/hosts.allow
 echo "sshd: $NETWORK" >> /etc/hosts.allow
 
 # Set time
--- scripts/on-rpi-boot.sh
+++ scripts/on-rpi-boot.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 NETWORK="$(/root/scripts/check-ipsubnet.sh $(ifconfig eth0 | awk '$0 ~ /Bcast/ { print $2, $NF }' | sed -e 's/addr://g' -e 's/Mask://g'))"
+grep -v ^sshd: /etc/hosts.allow > /etc/hosts.allow-new
+mv /etc/hosts.allow-new /etc/hosts.allow
 echo "sshd: $NETWORK" >> /etc/hosts.allow
 
