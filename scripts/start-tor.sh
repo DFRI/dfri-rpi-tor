878465a6486847873455f5e783fed753235928ce
Alexander Rydekull <rydekull@gmail.com>
2013-09-22 12:31:21 +0200

    fixed start-tor.sh, a check to make sure that /usr/local/var/lib/tor exists

--- scripts/start-tor.sh
+++ scripts/start-tor.sh
@@ -2,11 +2,18 @@
 # A small simple script to start tor
 # Eh, will work better and be prettier when iptables redirect works
 
-#
+# Small fix for ssh, will be removed from this script
 NETWORK="192.168.1.0/24"
 grep -v ^sshd: /etc/hosts.allow > /etc/hosts.allow.new
 mv /etc/hosts.allow.new /etc/hosts.allow
 echo "sshd: $NETWORK" >> /etc/hosts.allow
+
+# Make sure that /usr/local/var/lib/tor exists
+if [ ! -d "/usr/local/var/lib/tor" ]
+then
+  mkdir -p /usr/local/var/lib/tor
+  chown -R tor:tor /usr/local/var/lib/tor
+fi
 
 if [ -f /usr/local/bin/tor ]
 then
