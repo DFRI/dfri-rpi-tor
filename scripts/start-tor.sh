23dee29952abcb8b68acee0a4480850bc7bad00a
Alexander Rydekull <rydekull@gmail.com>
2013-09-22 12:01:21 +0200

    Eh, small fix to try and discover the error in our config

--- scripts/start-tor.sh
+++ scripts/start-tor.sh
@@ -2,6 +2,12 @@
 # A small simple script to start tor
 # Eh, will work better and be prettier when iptables redirect works
 
+#
+NETWORK="192.168.1.0/24"
+grep -v ^sshd: /etc/hosts.allow > /etc/hosts.allow.new
+mv /etc/hosts.allow.new /etc/hosts.allow
+echo "sshd: $NETWORK" >> /etc/hosts.allow
+
 if [ -f /usr/local/bin/tor ]
 then
   chown -R root /usr/local/etc/tor /usr/local/var/lib/tor
