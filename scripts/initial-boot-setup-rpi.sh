8979a6ff722b7e23d819261c6e3f69b612918606
Alexander Rydekull <rydekull@gmail.com>
2013-09-22 13:48:50 +0200

    Fixed small bug in new script

--- scripts/initial-boot-setup-rpi.sh
+++ scripts/initial-boot-setup-rpi.sh
@@ -29,7 +29,7 @@ fi
 echo "ALL: ALL" >> /etc/hosts.deny
 
 # Fix hosts.allow
-NETWORK="$(/root/scripts/check-ipsubnet.sh $(ifconfig eth0 | awk '{ print $2, $NF }' | sed -e 's/addr://g' -e 's/Mask://g')"
+NETWORK="$(/root/scripts/check-ipsubnet.sh $(ifconfig eth0 | awk '$0 ~ /Bcast/ { print $2, $NF }' | sed -e 's/addr://g' -e 's/Mask://g'))"
 echo "sshd: $NETWORK" >> /etc/hosts.allow
 
 # Set time
--- scripts/on-rpi-boot.sh
+++ scripts/on-rpi-boot.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-NETWORK="$(/root/scripts/check-ipsubnet.sh $(ifconfig eth0 | awk '{ print $2, $NF }' | sed -e 's/addr://g' -e 's/Mask://g')"
+NETWORK="$(/root/scripts/check-ipsubnet.sh $(ifconfig eth0 | awk '$0 ~ /Bcast/ { print $2, $NF }' | sed -e 's/addr://g' -e 's/Mask://g'))"
 echo "sshd: $NETWORK" >> /etc/hosts.allow
 
