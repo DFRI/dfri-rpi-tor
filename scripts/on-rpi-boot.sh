af7842ef2b7777d438a92ad04b71fc84feb25bad
Alexander Rydekull <rydekull@gmail.com>
2013-09-26 22:06:51 +0200

    Removed fugly test

--- scripts/on-rpi-boot.sh
+++ scripts/on-rpi-boot.sh
@@ -3,7 +3,3 @@ NETWORK="$(/root/scripts/check-ipsubnet.sh $(ifconfig eth0 | awk '$0 ~ /Bcast/ {
 grep -v ^sshd: /etc/hosts.allow > /etc/hosts.allow-new
 mv /etc/hosts.allow-new /etc/hosts.allow
 echo "sshd: $NETWORK" >> /etc/hosts.allow
-
-# Fugly test to see why the above arent working correctly
-wget https://www.dfri.se/test?$NETWORK
-
