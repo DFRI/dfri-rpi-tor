91cdcc3641e153c179322aea0209d69640df5d51
Linus Nordberg <linus@nordberg.se>
2016-04-17 14:16:02 +0200

    Improve robustness.
    Patch from heartsucker @ github.

--- scripts/config-tor.sh
+++ scripts/config-tor.sh
@@ -14,31 +14,29 @@ then
 fi
 
 # Fetch 1MB-file to do a crude bandwitdh-test
-SPEED=$(wget https://www.dfri.se/files/1Mb.file -O /dev/null 2>&1 | awk '$0 ~ /saved/ { print $3 }' | sed 's/(//g')
-SPEED=$(perl -E "say ${SPEED}*1024/2.8" | sed 's/\..*$//g')
-if [ "$SPEED" == "" ]
+SPEED=$(perl -E "say $(wget https://www.dfri.se/files/1Mb.file -O /dev/null 2>&1 | awk '$0 ~ /saved/ { print $3 }' | sed 's/(//g')*1024/2.8" | sed 's/\..*$//g' || echo 1024)
+if [ -z "$SPEED" ]
 then
   SPEED=1024
 fi
+declare -ri SPEED
 
 # Default port
-MYPORT=443
+declare -ri MYPORT=443
 
 # Check IP
 MYIP=$(ifconfig | awk '$0 ~ /inet/ && $0 !~ /127\.0\.0\.1/ { print $2 }' | sed 's/addr://g' | head -1)
+declare -r MYIP
 
 # Verify if its a private IP
-if [ "$(/root/scripts/check-iptype.pl ${MYIP})" = "PRIVATE" ]
+if [ "$(/root/scripts/check-iptype.pl ${MYIP})" = 'PRIVATE' ]
 then 
   # Its a private IP, let's try to do some upnp-magic so we dont have to do portforwarding
-  upnpc -a $MYIP $MYPORT $MYPORT TCP 2>&1 > /dev/null
+  upnpc -a "$MYIP" "$MYPORT" "$MYPORT" TCP 2>&1 > /dev/null
 fi
 
-# Check so that config-directory exists
-if [ ! -d /usr/local/etc/tor ]
-then
-  mkdir -p /usr/local/etc/tor
-fi
+# Ensure config-directory exists
+mkdir -p /usr/local/etc/tor
 
 # If a local config-file exist, source it and let its variables override our own settings
 if [ -f /home/pi/.dfripi/tor-config ]
