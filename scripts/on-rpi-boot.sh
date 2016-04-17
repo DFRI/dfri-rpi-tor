4a8f295614492501baef358804e1b736829d56c3
Linus Nordberg <linus@nordberg.se>
2016-04-17 14:25:14 +0200

    Improve robustness.
    Patch from heartsucker @ github.

--- scripts/on-rpi-boot.sh
+++ scripts/on-rpi-boot.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
+set -eu
+set -o pipefail
+
 NETWORK="$(/root/scripts/check-ipsubnet.sh $(ifconfig eth0 | awk '$0 ~ /Bcast/ { print $2, $NF }' | sed -e 's/addr://g' -e 's/Mask://g'))"
+declare -r NETWORK
+
 grep -v ^sshd: /etc/hosts.allow > /etc/hosts.allow-new
 mv /etc/hosts.allow-new /etc/hosts.allow
 echo "sshd: $NETWORK" >> /etc/hosts.allow
