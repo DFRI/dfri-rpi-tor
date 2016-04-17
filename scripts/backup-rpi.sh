c8875ec94cb93b1528b5268390526e238abaac7b
Linus Nordberg <linus@nordberg.se>
2016-04-17 14:12:23 +0200

    Improve robustness.
    Patch from heartsucker @ github.

--- scripts/backup-rpi.sh
+++ scripts/backup-rpi.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
+set -e
+set -o pipefail
+
 # Backup is good, this requires a USB-key, trying to figure out where it is and doing the backup there
+
 TARGET=$(mount | grep /dev/sd | awk '{ print $3 }')
 DATE=$(date '+%Y%m%d')
 TARGETDIR=${TARGET}/${DATE}-backup-dfri-rpi
