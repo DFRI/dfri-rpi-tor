05f79b1123678fdd7b113e929448643fffb370b7
Linus Nordberg <linus@nordberg.se>
2016-04-17 14:30:34 +0200

    Improve robustness.
    Patch from heartsucker @ github.

--- scripts/wipe-rpi.sh
+++ scripts/wipe-rpi.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
+set -e
+
+declare -i is_failure=0
+
 # This will remove the node-specific stuff from the rpi
-rm -rf /etc/ssh/*key*
-rm -rf /root
-rm -rf /home/pi
-rm -rf /usr/local/var/lib/tor
-rm -rf /usr/local/var/lib/torrc
-apt-get clean
-mkdir /root /home/pi
-cp -pr /etc/skel/.* /home/pi
+rm -rf /etc/ssh/*key* /root /home/pi /usr/local/var/lib/tor /usr/local/var/lib/torrc || { is_failure=1; }
+apt-get clean || { is_failure=1; }
+mkdir /root /home/pi && cp -pr /etc/skel/.* /home/pi || { is_failure=1; }
+
+if [ ${is_failure} -ne 0 ]
+then
+  exit 1
+else
+  exit 0
+fi
