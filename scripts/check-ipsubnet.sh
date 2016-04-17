af2ce90069a7abf1c4eb1c721a09d4de681ec976
Linus Nordberg <linus@nordberg.se>
2016-04-17 14:13:34 +0200

    Improve robustness.
    Also fix a but with printing the IP address.
    
    Patch from heartsucker @ github.

--- scripts/check-ipsubnet.sh
+++ scripts/check-ipsubnet.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
-IP=$1
-NETMASK=$2
+set -e
+
+declare -r IP="$1"
+declare -r NETMASK="$2"
+
+:${IP:?'Arg 1 (IP) was not set'}
+:${NETMASK:?'Arg 2 (NETMASK) was not set'}
+
 IFS=. read -r i1 i2 i3 i4 <<< "$IP"
 IFS=. read -r m1 m2 m3 m4 <<< "$NETMASK"
-printf "%d.%d.%d.%d" "$((i1 & m1))" "$(($i2 & m2))" "$((i3 & m3))" "$((i4 & m4))"
+printf "%d.%d.%d.%d" "$((i1 & m1))" "$((i2 & m2))" "$((i3 & m3))" "$((i4 & m4))"
 printf "/"$NETMASK
